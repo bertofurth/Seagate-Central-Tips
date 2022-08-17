64ebef1a61364e2f252e445fc2a16f79a0a76cb8
Berto Furth <bfgithub@veryspeedy.net>
2022-08-17 21:31:42 +1000

    Changed logging

--- S90change-root-pw.sh
+++ S90change-root-pw.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "CHANGING ROOT PASSWORD"
+echo "Changing root password" > /dev/kmsg
 echo "root:XXXXX" | chpasswd
 pwconv
 cp /etc/passwd /usr/config/backupconfig/etc/passwd
 cp /etc/shadow /usr/config/backupconfig/etc/shadow   
+echo "Finished changing root password" > /dev/kmsg
